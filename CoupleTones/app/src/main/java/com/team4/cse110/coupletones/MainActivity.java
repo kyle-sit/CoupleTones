dcbdcefb739043bebca2752f48278a1e282418ad
Niral Pathak <niralpathak@gmail.com>
2016-05-05 17:23:01 -0700

    JIRA Issues Done - Create a Favorite Location Class; Create UI for adding favorite location using Google Maps API

--- CoupleTones/app/src/main/java/com/team4/cse110/coupletones/MainActivity.java
+++ CoupleTones/app/src/main/java/com/team4/cse110/coupletones/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         catch (InstantiationException | IllegalAccessException e)
         {
-            e.printStackTrace();
+            e.printStackTrace(); 
         }
 
 
