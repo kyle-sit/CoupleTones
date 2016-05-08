0741c6384f728c51e5fa4eac05cca607aeded377
Niral Pathak <niralpathak@gmail.com>
2016-05-08 14:27:54 -0700

    working on test cases for favorite locations

--- CoupleTones/app/src/androidTest/java/com/team4/cse110/coupletones/Tests/FavoriteLocationTests.java
+++ CoupleTones/app/src/androidTest/java/com/team4/cse110/coupletones/Tests/FavoriteLocationTests.java
@@ -1,4 +1,4 @@
-package com.team4.cse110.coupletones;
+package com.team4.cse110.coupletones.Tests;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -15,27 +15,41 @@ public class FavoriteLocationTests extends TestCase
 {
     FavoriteLocation favoriteLocation = new FavoriteLocation();
 
-    /*if this passes, then default constructor works*/
-    public void testNull(){
+    /*
+    if this passes, then default constructor works
+    */
+    public void test_Null()
+    {
         assertNotNull(favoriteLocation);
     }
 
-    //faild because of memory address
-    public void testMarkerOption(){
+    //fails because of memory address
+    public void test_MarkerOption(){
         MarkerOptions marker = new MarkerOptions().position(new LatLng(0,0)).title("").snippet("");
         assertEquals(marker,favoriteLocation.getMarkerOptions());
     }
 
-    public void testPosition(){
+    /*
+    testing the getPosition method we defined
+     */
+    public void test_Position()
+    {
         MarkerOptions marker = new MarkerOptions().position(new LatLng(0,0)).title("N/A").snippet("N/A");
         assertEquals(marker.getPosition(),favoriteLocation.getMarkerOptions().getPosition());
     }
 
-    public void testSnippet(){
+    /*
+    testing the getSnippet and getMarkerOptions Methds
+     */
+    public void test_Snippet(){
         String s = favoriteLocation.getMarkerOptions().getSnippet();
         assertEquals("N/A",s);
     }
-    public void testTitle(){
+
+    /*
+    testing the title setting and getting for our favorite location title
+     */
+    public void test_Title(){
         assertEquals("N/A",favoriteLocation.getTitle());
     }
 
@@ -43,7 +57,7 @@ public class FavoriteLocationTests extends TestCase
       but we didnt initiallize because we dont initiallize
       marker in drfault constructor
     */
-    public void testSetTitle(){
+    public void test_SetTitle(){
         favoriteLocation.editName("test");
         assertEquals("test",favoriteLocation.getTitle());
     }
